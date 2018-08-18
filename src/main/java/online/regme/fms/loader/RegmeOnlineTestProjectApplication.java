a74f145a3f36c9c4c9bca0509800c454e0b425ac
Alex <panthrashkov@gmail.com>
2018-08-18 13:51:37 +0300

    First commit

---
+++ src/main/java/online/regme/fms/loader/RegmeOnlineTestProjectApplication.java
@@ -0,0 +1,12 @@
+package online.regme.fms.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegmeOnlineTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegmeOnlineTestProjectApplication.class, args);
+	}
+}
