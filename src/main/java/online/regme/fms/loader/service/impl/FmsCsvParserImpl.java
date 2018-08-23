25804c6c3cc37f92e9ef21374f50a693355cc14b
APankov <panthrashkov@rambler.ru>
2018-08-23 16:52:35 +0300

    csv parser

--- src/main/java/online/regme/fms/loader/service/impl/FmsCsvParserImpl.java
+++ src/main/java/online/regme/fms/loader/service/impl/FmsCsvParserImpl.java
@@ -3,35 +3,54 @@ package online.regme.fms.loader.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import online.regme.fms.loader.model.Fms;
 import online.regme.fms.loader.service.FmsCsvParser;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 @Service
 @Slf4j
 public class FmsCsvParserImpl implements FmsCsvParser {
 
+    //    @Override
+//    public List<Fms> getFmsList(String fileLocation) {
+//
+//        List<Fms> fms = new ArrayList<>();
+//        try {
+//
+//            Scanner scanner = new Scanner(new File(fileLocation));
+//            String version = scanner.nextLine().split(",")[1].trim();
+//            while (scanner.hasNext()) {
+//                String[] data = scanner.nextLine().split(",");
+//                fms.add(new Fms(data[0].trim(), data[1].trim(), Integer.valueOf(version)));
+//            }
+//            scanner.close();
+//        } catch (IOException e) {
+//            log.error("Excel parse error", e.getMessage());
+//        }
+//
+//        return fms;
+//    }
     @Override
     public List<Fms> getFmsList(String fileLocation) {
-
         List<Fms> fms = new ArrayList<>();
         try {
-
-            Scanner scanner = new Scanner(new File(fileLocation));
-            String version = scanner.nextLine().split(",")[1].trim();
-            while (scanner.hasNext()) {
-                String[] data = scanner.nextLine().split(",");
-                fms.add(new Fms(data[0].trim(), data[1].trim(), Integer.valueOf(version)));
+            final Reader reader = new InputStreamReader(new FileInputStream(new File(fileLocation)), "WINDOWS-1251");
+            List<CSVRecord> records = CSVFormat.EXCEL.parse(reader).getRecords();
+            String version =  records.get(0).get(1).replaceAll("\"", "").trim();
+            records.remove(0);
+            records.remove(0);
+            for (CSVRecord record : records) {
+                String name = record.get(0);
+                String code = record.get(1);
+                fms.add(new Fms(code, name, Integer.valueOf(version)));
             }
-            scanner.close();
         } catch (IOException e) {
-            log.error("Excel parse error", e.getMessage());
+            e.printStackTrace();
         }
-
         return fms;
     }
 }
--- src/test/java/online/regme/fms/loader/service/impl/FmsCsvParserTest.java
+++ src/test/java/online/regme/fms/loader/service/impl/FmsCsvParserTest.java
@@ -8,12 +8,15 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class FmsExcelParserTest {
+public class FmsCsvParserTest {
+
     private FmsCsvParser fmsExcelParser = new FmsCsvParserImpl();
+    private static final String FILE_PATH = "fms_structure_10012018.csv";
 
     @Test
     public void testParseFile(){
-        List<Fms> list = fmsExcelParser.getFmsList("fms_structure_10012018.csv");
-        assertEquals(4, list.size());
+        List<Fms> list = fmsExcelParser.getFmsList(getClass().getResource(FILE_PATH).getFile());
+        assertEquals(81, list.size());
+        assertEquals(" \"580-001\"".)
     }
 }
